317dc6dd424f4cdcf2302ced9d37c7f1b429900c
Samina Akhmetzyanova <samina.akhmetzyanova@mail.ru>
2025-09-18 12:11:54 +0300

    feat: add getRepository and getRepositoryReadme methods in AppRepository with centralized auth header.

--- app/src/main/java/com/example/repoviewer/data/repository/AppRepository.kt
+++ app/src/main/java/com/example/repoviewer/data/repository/AppRepository.kt
@@ -2,6 +2,7 @@ package com.example.repoviewer.data.repository
 
 import com.example.repoviewer.data.network.GithubApi
 import com.example.repoviewer.data.network.Repo
+import com.example.repoviewer.data.network.RepoDetails
 import com.example.repoviewer.data.storage.KeyValueStorage
 import com.example.repoviewer.data.network.UserInfo
 import javax.inject.Inject
@@ -10,6 +11,11 @@ class AppRepository @Inject constructor(
     private val api: GithubApi,
     private val storage: KeyValueStorage
 ) {
+    private fun getAuthHeader(): String {
+        val token = storage.authToken!!
+        return "token $token"
+    }
+
     suspend fun signIn(token: String): UserInfo {
         val authToken = "token $token"
         val userInfo = api.getUser(authToken)
@@ -20,8 +26,22 @@ class AppRepository @Inject constructor(
     }
 
     suspend fun getRepositories(): List<Repo> {
-        val token = storage.authToken!!
-        val authHeader = "token $token"
-        return api.getUserRepositories(authHeader, perPage = 10)
+        return api.getUserRepositories(getAuthHeader(), perPage = 10)
+    }
+
+    suspend fun getRepository(repoId: String): RepoDetails {
+        val (owner, repoName) = repoId.split("/")
+        return api.getRepository(getAuthHeader(), owner, repoName)
+    }
+
+    suspend fun getRepositoryReadme(
+        ownerName: String,
+        repositoryName: String,
+        branchName: String
+    ): String {
+        val readmeResponse = api.getRepositoryReadme(getAuthHeader(), ownerName, repositoryName)
+        val decodedBytes =
+            android.util.Base64.decode(readmeResponse.content, android.util.Base64.DEFAULT)
+        return String(decodedBytes, Charsets.UTF_8)
     }
 }
